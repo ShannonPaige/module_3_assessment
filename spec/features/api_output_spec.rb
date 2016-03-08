e79eaf840343be49c0996432aaba0dcf83be82d7
ShannonPaige <spaige@gmail.com>
2016-03-08 09:32:47 -0700

    WIP: 15 min commit

--- spec/features/api_output_spec.rb
+++ spec/features/api_output_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::ItemsController, type: :controller do
+describe "api" do
 
+  let!(:item) {Item.create(name: "item_name",
+                          description: "item_description",
+                          image_url: "image_url",
+                          created_at: "2016-03-08 04:21:31",
+                          updated_at: "2016-03-14 04:21:31")}
 
+    it "returns a list of items" do
+      visit '/api/v1/items'
 
+      expect(page).to have_content "item_name"
+      expect(page).to have_content "item_description"
+      expect(page).to have_content "item_url"
+      expect(page).not_to have_content "2016-03-08 04:21:31"
+      expect(page).not_to have_content "2016-03-14 04:21:31"
+
+    end
 end

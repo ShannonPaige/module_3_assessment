bb3af2dac802aedd98443317a5568ec028e2ffa1
ShannonPaige <spaige@gmail.com>
2016-03-07 22:04:16 -0700

    Complete setup for M3 assessment.

--- Gemfile
+++ Gemfile
@@ -12,13 +12,34 @@ gem "faker"
 gem "haml-rails"
 gem "therubyracer"
 gem "less-rails-bootstrap"
-gem "minitest-rails"
+gem "sdoc", "~> 0.4.0", group: :doc
+gem "figaro"
+gem "pg"
+gem "responders"
+gem "active_model_serializers", github: "rails-api/active_model_serializers"
+gem "faraday"
 
 group :development do
   gem "spring"
+  gem "web-console", "~> 2.0"
+  gem "quiet_assets"
 end
 
 group :development, :test do
-  gem "minitest-rails-capybara"
-  gem "pry", :require => "pry"
+  gem "byebug"
+  gem "pry"
+  gem "rspec-rails", "~> 3.0"
+  gem "capybara"
+  gem "database_cleaner"
+  gem "launchy"
+  gem "mocha"
+  # gem "selenium-webdriver"
+  # gem "factory_girl_rails", "~> 4.0"
+  # gem "shoulda-matchers", "~> 3.1"
+end
+
+group :test do
+  gem 'simplecov', require: false
+  # gem "vcr"
+  # gem "webmock"
 end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,47 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
+require "database_cleaner"
+require "simplecov"
+
+SimpleCov.start "rails"
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include Capybara::DSL
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do |example|
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+def json_response
+  JSON.parse(response.body)
+end
